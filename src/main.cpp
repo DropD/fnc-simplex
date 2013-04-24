b1680810c46eddc4b6b3c02d5a22a5e89526aaea
Rico Häuselmann <r.haeuselmann@gmx.ch>
2013-04-24 18:30:15 +0200

    add file stubs for main.cpp and simplex.h

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*! \file main.cpp
+ * Simplex Application Main
+ * solves a linear program in standard form
+ * i.e.
+ * max c' x
+ * A x = b
+ * x >= 0
+ */
+#include "../include/simplex.h"
+
+int main (int argc, char const* argv[])
+{
+    
+    return 0;
+}
