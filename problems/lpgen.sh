6cab3ddcc69dcba7d70a6078f3533f5258d5b0b0
Donjan Rodic <donjan@dyx.ch>
2013-05-20 08:25:47 +0200

    updated ignores

--- problems/lpgen.sh
+++ problems/lpgen.sh
@@ -19,5 +19,5 @@ for i in {1..20}; do $LPGEN 100 gen/lpb_1e2_$"$i" ; done
 echo generate 5 lps of size 500
 for i in {1..5}; do $LPGEN 500 gen/lpb_5e2_$"$i" ; done
 
-echo generate 1 lp of size 1000
-for i in {1..1}; do $LPGEN 1000 gen/lpb_1e3_$"$i" ; done
+echo generate 3 lp of size 1000
+for i in {1..3}; do $LPGEN 1000 gen/lpb_1e3_$"$i" ; done
