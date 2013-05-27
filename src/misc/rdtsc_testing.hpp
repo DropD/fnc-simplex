b2db893b4ea6d52e1fd4bf3010c766d8ec3a3ed2
Donjan Rodic <donjan@dyx.ch>
2013-05-27 02:08:13 +0200

    added gurobi

--- src/misc/rdtsc_testing.hpp
+++ src/misc/rdtsc_testing.hpp
@@ -12,6 +12,7 @@
 #include "rdtsc.h"  /* actual timing macros */
 #include "../simplex/Simplex.hpp"
 #include <glpk.h>
+#include <gurobi_c++.h>
 #include <iostream>
 
 #ifndef RDTSC_CYCLES_REQUIRED
@@ -70,6 +71,33 @@ int rdtsc_warmup(glp_prob * lp, glp_smcp * parm, std::string fname) {
   return num_runs;
 }
 
+int rdtsc_warmup(GRBEnv & env, std::string fname) {
+  if(RDTSC_CYCLES_REQUIRED <= 1) return 1;
+  double cycles = 0;
+  tsc_counter start, end;
+  int i;
+  int num_runs = 1;
+  CPUID(); RDTSC(start); RDTSC(end);
+  CPUID(); RDTSC(start); RDTSC(end);
+  CPUID(); RDTSC(start); RDTSC(end);
+  while(1) {
+    for(i = 0; i < num_runs; i++) {
+      GRBModel model = GRBModel(env, fname);
+      CPUID(); RDTSC(start);
+      model.optimize();
+      RDTSC(end); CPUID();
+      cycles += ((double)COUNTER_DIFF(end, start));
+    }
+    if(cycles >= RDTSC_CYCLES_REQUIRED) break;
+    num_runs *= 2;
+  }
+  return num_runs;
+}
+
+
+
+
+
 template <typename T>
 double rdtsc_measure(int num_runs, SimplexBase<T> * s, std::string fname) {
   double cycles = 0;
@@ -96,6 +124,22 @@ double rdtsc_measure(int num_runs, glp_prob * lp, glp_smcp * parm, std::string f
     glp_read_lp(lp, NULL, fname.c_str());
     CPUID(); RDTSC(start);
     glp_simplex(lp, parm);
+    RDTSC(end); CPUID();
+    cycles += ((double)COUNTER_DIFF(end, start));
+  }
+  cycles = cycles / ((double) num_runs);
+  return cycles;
+}
+
+double rdtsc_measure(int num_runs, GRBEnv & env, std::string fname) {
+  double cycles = 0;
+  tsc_counter start, end;
+  int i;
+
+  for(i = 0; i < num_runs; i++) {
+    GRBModel model = GRBModel(env, fname);
+    CPUID(); RDTSC(start);
+    model.optimize();
     RDTSC(end); CPUID();
     cycles += ((double)COUNTER_DIFF(end, start));
   }
---
+++ src/run_gurobi.hpp
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * 
+ * Running Gurobi on specified input file
+ * 
+ ******************************************************************************/
+
+#include <iostream>
+#include <string>
+
+#include "simplex/baseline.hpp"
+#include "../../gurobi/include/gurobi_c++.h"
+
+using namespace std;
+typedef double s_type;
+typedef unsigned int uint;
+
+void run_gurobi(string fname, SimplexBase<s_type> * s) {
+
+    cout << "\033[0;31m" << "Running " << "Gurobi" << "\033[0m" << std::endl;
+
+    if(INFO) {
+        s->load(fname);
+        s->print();
+    }
+    try {
+
+        GRBEnv env = GRBEnv();
+        env.set(GRB_IntParam_Threads, 1);
+        env.set(GRB_IntParam_Presolve, 0);
+        //~ env.set(GRB_IntParam_LogToConsole, 0);
+
+        //~ GRBModel model = GRBModel(env, fname);
+        //~ model.optimize();
+        int n = rdtsc_warmup(env, fname);
+        double cycles = rdtsc_measure(n, env, fname);    // solve
+
+        double fpc = (s->PERFC_ADDMUL + s->PERFC_DIV) / cycles;
+        double ci = (s->PERFC_ADDMUL+s->PERFC_DIV)/8./s->PERFC_MEM;
+
+        cout << "Iterations: " << s->get_iter() << endl;
+        cout << "Memory used: " << s->memusage() << " kB" << endl;
+        cout << "RDTSC cycles: " << cycles << " (avg over " << n << " runs)" << endl;
+        cout << "Memory accesses: " << s->PERFC_MEM
+             << " (theory: " << s->get_iter() * s->get_tabn() << ")" << endl;
+        cout << "Float add/mul: " << s->PERFC_ADDMUL << endl;
+        cout << "Float div: " << s->PERFC_DIV << endl;
+        cout << "FLOP/C: " << fpc << endl;
+        cout << "Op Intensity: " << ci << endl;
+
+        ofstream fp("rdtsc", fstream::app);
+        if(fp.is_open()) {
+            fp << "gurobi" << ','
+               << cycles << ','
+               << fpc << ','
+               << ci << endl;
+            fp.close();
+        } else
+            cout << "Error: unable to write to file rdtsc!" << endl;
+
+    } catch(GRBException e) {
+        cout << "Error code = " << e.getErrorCode() << endl;
+        cout << e.getMessage() << endl;
+    } catch (...) {
+        cout << "Error during optimization" << endl;
+    }
+
+}
