303a52379cd2be4874619b8158850bb36c72ddfb
Donjan Rodic <donjan@dyx.ch>
2013-05-28 16:25:30 +0200

    small testing change

--- problems/lptest.sh
+++ problems/lptest.sh
@@ -1,8 +1,11 @@
 
 PROB=$1
+
 time ../../cpplex/cpplex-read-only/bin/solver "$PROB".cpplp
 echo "______________________________________________"
 time ../../gurobi/run_gurobi.sh "$PROB".lp
+echo "______________________________________________"
+time ../../soplex/bin/soplex "$PROB".lp
 echo "______________________________________________"
 glpsol  --lp "$PROB".lp
 echo "______________________________________________"
